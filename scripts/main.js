4aff0a0d757514e692b656a344368a29a1b2513f
Ryzlane <ryzlane.arsac@hetic.net>
2018-06-15 15:15:37 +0200

    cococo

--- scripts/main.js
+++ scripts/main.js
@@ -1,9 +1,28 @@
 const poster1 = document.querySelector('#poster-1');
 const poster2 = document.querySelector('#poster-2');
 const poster3 = document.querySelector('#poster-3');
+let cursor = document.querySelector('#cursor');
+
+let animCamera = document.querySelector("#camera-animation");
+
 
 // poster1.setAttribute('material', {
 //     src: '#hardrock-poster'
+//   });
+
+// AFRAME.registerComponent('handle-events', {
+//     init: function () {
+//     let cursorAnim = document.querySelector("#cursor-animation"); 
+
+//     this.el.addEventListener('mouseenter', function () {
+//         console.log(cursorAnim)
+
+//         // cursorAnim.emit("fusing-posters")  
+//       });
+//     //   el.addEventListener('mouseleave', function () {
+//     //     cursor.setAttribute('color', '#EF2D5E');  
+//     //   });
+//     } 
 //   });
 
 AFRAME.registerComponent("passthrough", {
